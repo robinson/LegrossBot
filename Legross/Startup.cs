227ce631bb592b463a2eaf7686ee6a23092f6482
giang <letrunghieu2710@hotmail.com>
2016-09-23 17:02:59 +0200

    adfdasfdsfasdf

---
+++ Legross/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Legross.Startup))]
+namespace Legross
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
